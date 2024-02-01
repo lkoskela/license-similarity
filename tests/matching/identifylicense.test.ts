ed16fa976f9bdc4fc91644a5cd18699e3342ec17
Lasse Koskela <lasse.koskela@gmail.com>
2024-02-01 23:05:32 +0200

    Add a couple of odd samples of the kind of license texts there is out there and fix the implementation that could not handle these cases

--- tests/matching/identifylicense.test.ts
+++ tests/matching/identifylicense.test.ts
@@ -1,4 +1,4 @@
-import { identifyLicense } from '../../src/index'
+import { identifyLicense, matchLicenses } from '../../src/index'
 import { samples } from '../samples/samples'
 import { Licenses } from '../../src/data/types'
 
@@ -13,6 +13,47 @@ describe('identifyLicense', () => {
 
     test.each(['Apache-2.0', '0BSD', 'MIT', 'GPL-3.0-or-later'])('%s', (licenseId: string) => {
         expect(identifyLicense(licenseTextFor(licenseId))).toStrictEqual(licenseId)
+    })
+
+    describe('The Unlicense', () => {
+        it('Unlicense text from the SPDX database', () => {
+            expect(identifyLicense(licenseTextFor('Unlicense'))).toStrictEqual('Unlicense')
+        })
+
+        it('Unlicense text from a Github repo', () => {
+            const text =
+                'This is free and unencumbered software released into the public domain.\n' +
+                '\n' +
+                'Anyone is free to copy, modify, publish, use, compile, sell, or\n' +
+                'distribute this software, either in source code form or as a compiled\n' +
+                'binary, for any purpose, commercial or non-commercial, and by any\n' +
+                'means.\n' +
+                '\n' +
+                'In jurisdictions that recognize copyright laws, the author or authors\n' +
+                'of this software dedicate any and all copyright interest in the\n' +
+                'software to the public domain. We make this dedication for the benefit\n' +
+                'of the public at large and to the detriment of our heirs and\n' +
+                'successors. We intend this dedication to be an overt act of\n' +
+                'relinquishment in perpetuity of all present and future rights to this\n' +
+                'software under copyright law.\n' +
+                '\n' +
+                'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,\n' +
+                'EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF\n' +
+                'MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.\n' +
+                'IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR\n' +
+                'OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,\n' +
+                'ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR\n' +
+                'OTHER DEALINGS IN THE SOFTWARE.\n' +
+                '\n' +
+                'For more information, please refer to <http://unlicense.org/>'
+
+            // console.warn(`From Github: ${text}\nFrom SPDX: ${licenseTextFor('Unlicense')}`)
+            // console.warn(`matchLicenses when forceCharacterBasedDice: false => ${JSON.stringify(matchLicenses(text, { forceCharacterBasedDice: false, threshold: 0.75 }), null, 2)}`)
+            // console.warn(`matchLicenses when forceCharacterBasedDice: true => ${JSON.stringify(matchLicenses(text, { forceCharacterBasedDice: true, threshold: 0.75 }), null, 2)}`)
+
+            expect(identifyLicense(text, { forceCharacterBasedDice: true, threshold: 0.75, max: 1 })).toStrictEqual('Unlicense')
+
+        })
     })
 
     describe('GPL-3.0 and GPL-3.0+', () => {
